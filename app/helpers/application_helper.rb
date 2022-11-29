c70703a1237b205dd0d39550e7e8e9ebce1e9fc0
AnejaPirnat <aneja.pirnat@scv.si>
2022-11-29 09:28:29 +0100

    helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,17 @@ module ApplicationHelper
     def display_alert
         content_tag(:p, alert, class: "alert")
     end
+
+    def display_navbar_options
+        if user_signed_in?
+            render 'loggedInNav'
+        else
+            render 'loggedOutNav'
+        end
+    end
+    
+    
+    def display_active_class(link_path)
+        "active" if current_page?(link_path)
+    end
 end

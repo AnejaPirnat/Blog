c72b3dca007ab2a848ddda8fc1c95381beffafc8
AnejaPirnat <aneja.pirnat@scv.si>
2022-11-29 09:24:51 +0100

    navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def display_notice
+        content_tag(:p, notice, class: "notice")
+    end
+
+    def display_alert
+        content_tag(:p, alert, class: "alert")
+    end
 end

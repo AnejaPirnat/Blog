19994409c770997d33856e58ce28c816c4a8f85a
AnejaPirnat <aneja.pirnat@scv.si>
2023-01-05 09:31:27 +0100

    frontend

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ApplicationRecord
     validates_presence_of :title, :body
     has_many :comments
     scope :published, -> { where(published_at: nil).or(where("published_at <= ?", DateTime.now)) }
+    has_rich_text :body
 end

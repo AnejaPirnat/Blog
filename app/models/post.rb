5f1b58e21bda46c7955a6ed59aa3f2804266c9d4
AnejaPirnat <aneja.pirnat@scv.si>
2023-01-05 08:06:25 +0100

    comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
     belongs_to :user
     validates_presence_of :title, :body
     has_many :comments
-    scope :published, -> {where(published_a: nil).or(where("published_at <= ?", DateTime.Now))}
+    scope :published, -> { where(published_at: nil).or(where("published_at <= ?", DateTime.now)) }
 end
